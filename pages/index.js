fafe81dea30ffde6b2754eb8bced2d32aae3897d
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-02-16 19:14:22 +0200

    initial cms addition

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { attributes, react as HomeContent } from '../content/home.md';
 
 export default function Home() {
+  let { title, cats } = attributes;
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
 
       <main>
@@ -16,6 +19,14 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
+      <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
 
       <Footer />
     </div>
