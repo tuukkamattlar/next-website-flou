98ed6416f387af19b57dd1de94ed27053a94fe24
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-02-26 17:15:05 +0200

    initialize osaaminen.js and path

--- pages/osaaminen.js
+++ pages/osaaminen.js
@@ -1,19 +1,14 @@
 import { attributes} from '../content/competences.md';
 import GeneralHeader from '@components/GeneralHeader';
+import SkillCategoryBox from '@components/SkillCategoryBox';
 
 export default function Projects({ lan }) {
     let { title, headerIMG, description, items } = attributes;
     return(
-        <div >
+        <div className='powder'>
             <GeneralHeader title={title} img={headerIMG} lan={lan} description={description}/>
-            <h1>{title[lan]}</h1>
-            <p>{description[lan]}</p>
-            {items.map((v,k) => (
-                <div key={k}>
-                    <a>{v.title[lan]}</a>
-                    <img src={v.skillIMG} width={300}></img>
-                </div>
-            ))}
+            <p>{description[lan]}</p>¨
+            <SkillCategoryBox items={items} lan={lan}/>
         </div>
     )
   }
@@ -23,7 +18,14 @@ export default function Projects({ lan }) {
 
 
 <SmallRef items={items}/>
-
+            <h1>{title[lan]}</h1>
+            <p>{description[lan]}</p>
+            {items.map((v,k) => (
+                <div key={k}>
+                    <a>{v.title[lan]}</a>
+                    <img src={v.skillIMG} width={300}></img>
+                </div>
+            ))}
 
 
 
