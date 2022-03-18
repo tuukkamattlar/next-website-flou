d814e5287e64eafe382831c93e4256437b454b94
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-03-18 14:35:05 +0200

    update contact

--- pages/yhteystiedot.js
+++ pages/yhteystiedot.js
@@ -1,15 +1,12 @@
 import { attributes } from '../content/contact.md';
+import ContactInfo from '@components/ContactInfo';
 import GeneralHeader from '@components/GeneralHeader';
 
 export default function Contact({ lan }) {
-    let {title, headerIMG, bodyObject, fillIMG} = attributes
     return(
         <div>
-            <GeneralHeader title={title} img={headerIMG} lan={lan} description={''}/>
-            <h1>{title[lan]}</h1>
-            <img src={headerIMG} width={300}></img>
-            <p>{bodyObject[lan]}</p>
-            <img src={fillIMG} width={300}></img>
+            <GeneralHeader title={attributes.title} img={attributes.headerIMG} lan={lan} description={''}/>
+            <ContactInfo item={attributes} lan={lan}/>
         </div>
     )
   }
