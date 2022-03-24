648d453aa5c00a1b10a91ba4b3ec59b21e2cad26
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-03-24 11:20:37 +0200

    update to blog content

--- components/BlogBox.js
+++ components/BlogBox.js
@@ -1,14 +1,15 @@
 import styles from './styles/BlogBox.module.css'
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
+import Markdown from 'markdown-to-jsx';
 
-export default function BlogBox({ items, lan }) {
+export default function BlogBox({ items, description, lan }) {
   const [years, setYears] = useState([])
   console.log(items)
   return (
       <div className={styles.container} >
         <div className={styles.intro}>
-          <p>{'content'}</p>
+          <Markdown>{description[lan]}</Markdown>
         </div>
           {items.map((item, k) => (
             <Link href={'/blogi/'+item.url} key={k}>
--- pages/blogi.js
+++ pages/blogi.js
@@ -3,11 +3,11 @@ import GeneralHeader from '@components/GeneralHeader';
 import BlogBox from '@components/BlogBox';
 
 export default function Blogi({ lan }) {
-    let { title, headerIMG, blogs } = attributes;
+    let { title, description, headerIMG, blogs } = attributes;
     return(
         <div className='powder'>
             <GeneralHeader title={title} img={headerIMG} lan={lan} description={''}/>
-            <BlogBox items={blogs} lan={lan} />
+            <BlogBox items={blogs} lan={lan} description={description} />
         </div>
     )
   }
