6924bb59ee1cb217bb9b0a45607d3f565a58b61a
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-05-03 11:44:31 +0300

    add balls module frame

---
+++ components/general_components/ShortBallsModule.js
@@ -0,0 +1,18 @@
+import ShortBallsCSS from './styles/ShortBalls.module.css'
+import Link from 'next/link';
+
+export default function ShortBallsModule({categories, lan}) {
+    return (
+            <div className={ShortBallsCSS.container}>
+            {categories.map((value, index) => (
+                <Link href={value.url} key={index}>
+                <div className={ShortBallsCSS.ball}>
+                    <h3>{value.title[lan]}</h3>
+                    <p>{value.short[lan]}</p>
+                </div>
+                </Link>
+            ))}
+        </div>
+    )
+  }
+  
--- pages/test.js
+++ pages/test.js
@@ -3,9 +3,11 @@ import GeneralHeader from "@components/GeneralHeader";
 import FullWidthTextModule from "@components/general_components/FullWidthTextModule";
 import TextAndImageModule from "@components/general_components/TextAndImageModule";
 import TitleModule from "@components/general_components/TitleModule";
+import ShortBallsModule from "@components/general_components/ShortBallsModule";
 
 // Content
 import { attributes as flou} from '../content/flou.md';
+import { attributes as categories} from '../content/competences_list.md';
 
 // Styles
 import GeneralCSS from '../components/general_components/styles/General.module.css'
@@ -32,6 +34,10 @@ export default function Test({lan}) {
         />
         <FullWidthTextModule 
           text={flou.description}
+          lan = {lan}
+        />
+        <ShortBallsModule 
+          categories={categories.category}
           lan = {lan}
         />
       </div>
