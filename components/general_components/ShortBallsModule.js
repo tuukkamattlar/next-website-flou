9d62629580553d3740f4742f4fa0b68b84c6c7d3
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-05-03 11:46:42 +0300

    fix routing for ball block

--- components/general_components/ShortBallsModule.js
+++ components/general_components/ShortBallsModule.js
@@ -5,7 +5,7 @@ export default function ShortBallsModule({categories, lan}) {
     return (
             <div className={ShortBallsCSS.container}>
             {categories.map((value, index) => (
-                <Link href={value.url} key={index}>
+                <Link href={'/osaaminen/'+value.url} key={index}>
                 <div className={ShortBallsCSS.ball}>
                     <h3>{value.title[lan]}</h3>
                     <p>{value.short[lan]}</p>
