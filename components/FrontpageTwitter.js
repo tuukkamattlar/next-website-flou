ec280b79534809766a045369cac5e2afc7700d82
Oliver Heinonen <oliver.heinonen@flou.io>
2022-05-24 13:08:56 +0300

    Twitter integration, api call params and rendering to be worked on

--- components/FrontpageTwitter.js
+++ components/FrontpageTwitter.js
@@ -1,39 +1,28 @@
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import { useContext, useEffect, useRef, useState } from 'react';
-import styles from '@components/styles/Home.module.css';
-import general from '@components/styles/FrontpageGeneral.module.css'
-import twitter from '@components/styles/Twitter.module.css'
+import general from '@components/styles/FrontpageGeneral.module.css';
+import twitter from '@components/styles/Twitter.module.css';
 import useWindowSize from '@components/hooks/useWindowSize';
-
-
 
 export default function FrontpageTwitter() {
   const size = useWindowSize();
   const ref = useRef();
-  const [twitterHeight, setTwitterHeight] = useState(1000);
-  const setHeightTweetsDiv = () => {
-    const height = ref?.current?.clientHeight;
-    setTwitterHeight(height);
-  };
+  const [tweets, setTweets] = useState([]);
+
   useEffect(() => {
-    setHeightTweetsDiv();
-  }, [size.width]);
-  const twitterProps = {
-    sourceType: 'profile',
-    screenName: 'flou_ltd',
-    options: { width: 500 },
-    noHeader: true,
-    noFooter: true,
-    autoHeight: true,
-    transparent: true
-  };
+    fetch('/get_tweets')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTweets(data);
+      });
+  }, []);
+
   return (
     <div className={general.pageHeightBox}>
-      <div className={styles.newscontainer}>
-      <h1>Twitter TODO</h1>
-        <div className={styles.twittercontainer} style={{ height: twitterHeight }}>
-            {size.width > 1250 ? <TwitterTimelineEmbed {...twitterProps} className={twitter.main}/> : ''}
-        </div>
+      <div className={twitter.twittercontainer}>
+        {tweets.map((tweet) => (
+          <h4>{tweet.text}</h4>
+        ))}
       </div>
     </div>
   );
