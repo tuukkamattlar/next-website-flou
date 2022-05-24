f94b3aad51e2adeeeed5af5cf19cdb0ad11ee9a7
lazautus2021 <80039245+lazautus2021@users.noreply.github.com>
2022-05-24 13:31:53 +0300

    Added animation to front page
    Animation to front page

---
+++ components/HomeAnimation.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import useWindowSize from './hooks/useWindowSize';
+
+export default function HomeAnimation({ src, className }) {
+  const size = useWindowSize();
+
+  const heightFactor = size.height > 750 ? -(1 + (10 * (size.height - 750)) / 750) * 5 : 1;
+
+  const right = Math.min(0.00000005 * Math.pow(size.width - 2000, 3) + heightFactor, 0);
+
+  if (size.width >= 750) {
+    return (
+      <iframe
+        style={{
+         
+       
+       
+          height: '100vh',
+          width: '100vw',
+          padding: '0px',
+          bottom: '0px',
+       
+        
+        }}
+        src={src}
+      />
+    );
+  }
+
+  return '';
+}
---
+++ public/img/animation/animate-smaller-2.js
@@ -0,0 +1,300 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop, this.reversed));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.wave_whitesvg = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// viiva1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.beginFill("#FFFFFF").beginStroke().moveTo(-626,538.3).curveTo(-616,521,-602.5,504.4).curveTo(-597.1,497.8,-590.5,490.7).lineTo(-577.3,477.5).curveTo(-539,441.9,-495.1,426).curveTo(-477.8,419.8,-457.3,415.6).curveTo(-445,413.1,-421.8,409.7).curveTo(-367.9,401.9,-337.8,386).curveTo(-325.6,379.6,-314.9,370.6).curveTo(-308.4,365.2,-297.1,353.9).curveTo(-285.9,342.7,-280.5,336.2).curveTo(-271.6,325.5,-265.1,313.3).curveTo(-249.1,283.1,-241.3,229.4).curveTo(-237.8,204.7,-235.1,191.9).curveTo(-230.6,170.3,-223.7,152.1).curveTo(-206,105.8,-165.5,65.4).curveTo(-125.1,25,-78.9,7.3).curveTo(-60.7,0.3,-39,-4.2).curveTo(-26.2,-6.9,-1.6,-10.4).curveTo(52.2,-18.2,82.3,-34.2).curveTo(94.5,-40.6,105.3,-49.5).curveTo(111.7,-54.9,123,-66.2).curveTo(134.2,-77.5,139.7,-83.9).curveTo(148.6,-94.6,155,-106.9).curveTo(171,-137,178.8,-190.8).curveTo(182.2,-214.5,184.8,-227.1).curveTo(189.1,-248,195.6,-265.6).curveTo(212.2,-310.5,249.5,-349.5).lineTo(249.8,-349.8).curveTo(288.8,-387,333.6,-403.7).curveTo(351.2,-410.2,372.2,-414.5).curveTo(384.6,-417,408.5,-420.5).curveTo(462.2,-428.3,492.4,-444.3).curveTo(504.6,-450.7,515.3,-459.6).curveTo(521.8,-464.9,533,-476.2).curveTo(546.5,-489.7,554.6,-501.3).curveTo(565.9,-517.4,573.6,-538.1).lineTo(626,-538.6).curveTo(615.7,-503.8,598.4,-478.1).curveTo(587.5,-461.9,567.4,-441.8).curveTo(554.2,-428.6,546.3,-422).curveTo(532.9,-410.8,517.8,-402.6).curveTo(479.4,-381.6,415.4,-372.4).curveTo(394,-369.3,383.3,-367.1).curveTo(365.7,-363.6,351.2,-358.3).curveTo(315.7,-345.2,284,-315.2).curveTo(254.1,-283.6,241,-248).curveTo(235.7,-233.6,232.2,-215.9).curveTo(230.1,-205.3,226.9,-183.8).curveTo(217.6,-119.8,196.7,-81.4).curveTo(188.4,-66.3,177.3,-53).curveTo(170.6,-45.1,157.4,-31.8).curveTo(144.1,-18.5,136.2,-11.9).curveTo(122.9,-0.8,107.8,7.5).curveTo(69.4,28.4,5.4,37.8).curveTo(-16.7,41,-27.8,43.2).curveTo(-46.2,46.9,-61.1,52.6).curveTo(-98,66.7,-131.2,99.9).curveTo(-164.2,132.9,-178.3,169.8).curveTo(-184,184.8,-187.8,203.1).curveTo(-190,214.1,-193.3,236.3).curveTo(-202.5,300.3,-223.5,338.7).curveTo(-231.7,353.8,-242.8,367.2).curveTo(-249.5,375.1,-262.7,388.3).curveTo(-276,401.6,-284,408.2).curveTo(-297.3,419.4,-312.3,427.7).curveTo(-350.8,448.6,-414.7,457.9).curveTo(-453.9,463.5,-473.7,470.1).curveTo(-506.3,481,-534.9,504.9).curveTo(-539.2,508.5,-544.2,513.1).lineTo(-554.9,523.9).curveTo(-560.9,530.3,-567.3,538.5).closePath();
+	this.shape.setTransform(-573.55,1616.25);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.beginFill("#FFFFFF").beginStroke().moveTo(-601.8,487.9).curveTo(-592.2,474.4,-573.8,456.1).curveTo(-560.5,442.8,-552.6,436.2).curveTo(-539.3,425,-524.2,416.8).curveTo(-485.7,395.8,-421.8,386.5).curveTo(-400.4,383.4,-389.6,381.3).curveTo(-372,377.7,-357.6,372.5).curveTo(-322,359.4,-290.4,329.4).curveTo(-260.3,297.8,-247.3,262.2).curveTo(-242,247.8,-238.5,230.1).curveTo(-236.4,219.5,-233.2,198).curveTo(-224,134,-203,95.6).curveTo(-194.8,80.5,-183.6,67.2).curveTo(-177,59.2,-163.7,46).curveTo(-150.4,32.7,-142.5,26.1).curveTo(-129.2,14.9,-114.1,6.7).curveTo(-75.7,-14.3,-11.7,-23.5).curveTo(10.5,-26.8,21.5,-29).curveTo(39.9,-32.8,54.8,-38.4).curveTo(91.8,-52.6,124.8,-85.6).curveTo(157.9,-118.7,172,-155.7).curveTo(177.7,-170.6,181.4,-188.9).curveTo(183.7,-199.9,186.9,-222.1).curveTo(196.2,-286.1,217.1,-324.5).curveTo(225.4,-339.6,236.5,-353).curveTo(243.2,-360.9,256.4,-374.1).curveTo(269.7,-387.4,277.6,-394).curveTo(290.9,-405.2,306,-413.4).curveTo(344.4,-434.4,408.4,-443.7).curveTo(429.5,-446.7,440.1,-448.8).curveTo(457.5,-452.3,471.7,-457.4).curveTo(506.7,-470,537.9,-499).lineTo(548.6,-509.7).curveTo(561.5,-523.6,571.6,-539.1).lineTo(627.3,-539).curveTo(619.4,-522.2,608.8,-507).curveTo(598,-491.4,584.2,-476.5).lineTo(571,-463.3).curveTo(532.7,-427.8,488.8,-411.8).curveTo(471.5,-405.6,451,-401.4).curveTo(438.7,-398.9,415.4,-395.5).curveTo(361.6,-387.7,331.5,-371.8).curveTo(319.3,-365.3,308.6,-356.4).curveTo(302.1,-351,290.8,-339.7).curveTo(279.6,-328.5,274.2,-322).curveTo(265.2,-311.3,258.8,-299.1).curveTo(242.8,-269,235,-215.2).curveTo(231.5,-190.7,228.8,-177.8).curveTo(224.3,-156.1,217.4,-137.9).curveTo(199.6,-91.7,159.2,-51.2).curveTo(118.8,-10.8,72.5,6.9).curveTo(54.3,13.9,32.7,18.4).curveTo(19.8,21.1,-4.7,24.6).curveTo(-58.5,32.4,-88.7,48.4).curveTo(-100.9,54.8,-111.6,63.7).curveTo(-118,69.1,-129.3,80.4).curveTo(-140.6,91.6,-146,98.1).curveTo(-154.9,108.8,-161.3,121).curveTo(-177.3,151.2,-185.1,205).curveTo(-188.5,228.8,-191.1,241.3).curveTo(-195.4,262.3,-201.9,279.8).curveTo(-218.5,324.7,-255.8,363.7).lineTo(-256.1,364).curveTo(-295.1,401.3,-339.9,417.9).curveTo(-357.5,424.4,-378.5,428.7).curveTo(-390.9,431.2,-414.8,434.7).curveTo(-468.6,442.5,-498.7,458.4).curveTo(-510.9,464.9,-521.6,473.8).curveTo(-528.1,479.2,-539.4,490.5).curveTo(-551.7,502.8,-558.1,510.9).curveTo(-568,523.5,-575,538.8).lineTo(-627.3,539.1).curveTo(-617.2,509.7,-601.8,487.9).closePath();
+	this.shape_1.setTransform(627.325,539.125);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-1199.5,0,2454.2,2154.8);
+
+
+(lib.viiva2 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// viiva2
+	this.shape = new cjs.Shape();
+	this.shape.graphics.beginFill("#FFFFFF").beginStroke().moveTo(-627.2,539.1).curveTo(-619.3,522.3,-608.7,507.1).curveTo(-597.9,491.5,-584,476.6).lineTo(-570.9,463.4).curveTo(-532.6,427.9,-488.6,411.9).curveTo(-471.4,405.7,-450.8,401.5).curveTo(-438.6,399,-415.3,395.6).curveTo(-361.5,387.8,-331.4,371.9).curveTo(-319.1,365.4,-308.4,356.5).curveTo(-302,351.1,-290.7,339.8).curveTo(-279.5,328.6,-274.1,322.1).curveTo(-265.1,311.4,-258.7,299.2).curveTo(-242.7,269.1,-234.9,215.3).curveTo(-231.4,190.8,-228.7,177.9).curveTo(-224.2,156.2,-217.3,138).curveTo(-199.5,91.8,-159.1,51.3).curveTo(-118.7,10.9,-72.4,-6.8).curveTo(-54.2,-13.8,-32.6,-18.3).curveTo(-19.7,-21,4.8,-24.5).curveTo(58.6,-32.3,88.8,-48.3).curveTo(101,-54.7,111.7,-63.6).curveTo(118.2,-69,129.4,-80.3).curveTo(140.7,-91.5,146.1,-98).curveTo(155,-108.7,161.4,-120.9).curveTo(177.4,-151.1,185.2,-204.9).curveTo(188.6,-228.7,191.2,-241.2).curveTo(195.5,-262.2,202,-279.7).curveTo(218.6,-324.6,255.9,-363.6).lineTo(256.2,-363.9).curveTo(295.2,-401.2,340,-417.8).curveTo(357.7,-424.3,378.6,-428.6).curveTo(391.1,-431.1,414.9,-434.6).curveTo(468.7,-442.4,498.9,-458.3).curveTo(511,-464.8,521.7,-473.7).curveTo(528.2,-479.1,539.5,-490.4).curveTo(552.3,-503.2,558.6,-511).curveTo(568.9,-524,575.9,-539.2).lineTo(627.2,-539).curveTo(617.1,-509.5,601.7,-487.8).curveTo(592.1,-474.2,573.9,-456).curveTo(560.6,-442.7,552.7,-436.1).curveTo(539.4,-424.9,524.3,-416.7).curveTo(485.8,-395.7,421.9,-386.4).curveTo(400.5,-383.3,389.8,-381.2).curveTo(372.1,-377.6,357.7,-372.4).curveTo(322.1,-359.3,290.5,-329.3).curveTo(260.4,-297.7,247.5,-262.1).curveTo(242.2,-247.7,238.6,-230).curveTo(236.5,-219.4,233.4,-197.9).curveTo(224.1,-133.9,203.1,-95.5).curveTo(194.9,-80.4,183.8,-67.1).curveTo(177.1,-59.1,163.9,-45.9).curveTo(150.6,-32.6,142.6,-26).curveTo(129.3,-14.8,114.3,-6.6).curveTo(75.8,14.4,11.9,23.6).curveTo(-10.4,26.9,-21.4,29.1).curveTo(-39.7,32.9,-54.6,38.5).curveTo(-91.7,52.7,-124.7,85.7).curveTo(-157.8,118.8,-171.8,155.8).curveTo(-177.6,170.7,-181.3,189).curveTo(-183.6,200,-186.8,222.2).curveTo(-196,286.2,-217,324.6).curveTo(-225.2,339.7,-236.4,353.1).curveTo(-243.1,361,-256.3,374.2).curveTo(-269.5,387.5,-277.5,394.1).curveTo(-290.8,405.3,-305.9,413.5).curveTo(-344.3,434.5,-408.3,443.8).curveTo(-429.4,446.8,-439.9,448.9).curveTo(-457.4,452.4,-471.5,457.5).curveTo(-506.6,470.1,-537.8,499.1).lineTo(-548.5,509.8).curveTo(-561.3,523.7,-571.4,539.2).closePath();
+	this.shape.setTransform(-161.45,1616.675);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.beginFill("#FFFFFF").beginStroke().moveTo(-625.6,538.4).curveTo(-615.4,503.5,-598.4,478.1).curveTo(-587.4,461.7,-567.8,442).curveTo(-554.5,428.8,-546.6,422.2).curveTo(-533.3,411,-518.2,402.8).curveTo(-479.8,381.8,-415.8,372.5).curveTo(-394.4,369.4,-383.6,367.3).curveTo(-366,363.7,-351.6,358.5).curveTo(-316,345.4,-284.4,315.4).curveTo(-254.4,283.8,-241.3,248.2).curveTo(-236.1,233.8,-232.5,216.1).curveTo(-230.4,205.5,-227.3,184).curveTo(-218,120,-197.1,81.6).curveTo(-188.8,66.5,-177.6,53.2).curveTo(-171,45.2,-157.7,32).curveTo(-144.5,18.7,-136.6,12.1).curveTo(-123.2,0.9,-108.1,-7.3).curveTo(-69.8,-28.3,-5.7,-37.6).curveTo(16.4,-40.8,27.5,-43).curveTo(45.9,-46.8,60.7,-52.5).curveTo(97.7,-66.6,130.8,-99.7).curveTo(163.8,-132.7,178,-169.7).curveTo(183.6,-184.6,187.4,-202.9).curveTo(189.6,-214,192.9,-236.2).curveTo(202.2,-300.1,223.1,-338.6).curveTo(231.3,-353.7,242.5,-367).curveTo(249.1,-374.9,262.4,-388.2).curveTo(275.7,-401.4,283.6,-408.1).curveTo(296.9,-419.2,312,-427.5).curveTo(350.4,-448.4,414.4,-457.7).curveTo(453.5,-463.3,473.4,-469.9).curveTo(506,-480.8,534.5,-504.7).curveTo(538.9,-508.4,543.8,-513).lineTo(554.5,-523.7).curveTo(560.5,-530.1,567,-538.4).lineTo(625.6,-538.2).curveTo(615.6,-520.8,602.1,-504.2).curveTo(596.7,-497.6,590.2,-490.6).lineTo(576.9,-477.4).curveTo(538.7,-441.8,494.8,-425.8).curveTo(477.5,-419.6,457,-415.4).curveTo(444.7,-412.9,421.4,-409.5).curveTo(367.6,-401.7,337.5,-385.8).curveTo(325.2,-379.4,314.5,-370.4).curveTo(308,-365,296.8,-353.8).curveTo(285.5,-342.5,280.1,-336).curveTo(271.2,-325.3,264.7,-313.1).curveTo(248.8,-282.9,241,-229.2).curveTo(237.5,-204.6,234.8,-191.8).curveTo(230.2,-170.1,223.3,-151.9).curveTo(205.6,-105.7,165.2,-65.3).curveTo(124.7,-24.8,78.5,-7.1).curveTo(60.3,-0.2,38.7,4.3).curveTo(25.9,7,1.2,10.6).curveTo(-52.5,18.4,-82.7,34.4).curveTo(-94.9,40.8,-105.6,49.7).curveTo(-112.1,55.1,-123.3,66.4).curveTo(-134.6,77.6,-140,84.1).curveTo(-149,94.8,-155.4,107).curveTo(-171.4,137.2,-179.1,191).curveTo(-182.6,214.7,-185.2,227.2).curveTo(-189.4,248.2,-196,265.8).curveTo(-212.5,310.6,-249.9,349.7).lineTo(-250.2,349.9).curveTo(-289.2,387.2,-333.9,403.8).curveTo(-351.5,410.4,-372.6,414.7).curveTo(-385,417.2,-408.8,420.7).curveTo(-462.6,428.5,-492.8,444.4).curveTo(-505,450.9,-515.7,459.8).curveTo(-522.1,465.1,-533.4,476.4).curveTo(-546.9,490,-555.2,501.6).curveTo(-566.6,517.8,-574.3,538.4).closePath();
+	this.shape_1.setTransform(1039.9,539.475);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.viiva2, new cjs.Rectangle(-788.6,1.1,2454.2,2154.8), null);
+
+
+(lib.viivatanim = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// viiva2
+	this.instance = new lib.viiva2();
+	this.instance.setTransform(30.05,3497.4,1,1,0,0,0,-758.6,2153.4);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).to({x:1023.95,y:2652.4},569).wait(1));
+
+	// viiva1
+	this.instance_1 = new lib.wave_whitesvg("synched",0);
+	this.instance_1.setTransform(270.15,2941.25,1,1,0,0,0,-1169.7,2153.2);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).to({x:878.25,y:2476.8},569).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,323.6,3448.1,3176.3);
+
+
+(lib.animfull = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.viivatanim();
+	this.instance.setTransform(1982.1,961.95,1,1,0,0,0,1982.1,1750);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.animfull, new cjs.Rectangle(0,0,2694.5,2711.9), null);
+
+
+// stage content:
+(lib.animatesmaller2 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_4
+	this.instance = new lib.animfull();
+	this.instance.setTransform(860.2,692.15,1,1,0,0,0,1347.2,1355.9);
+	this.instance.alpha = 0;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).to({alpha:1},20).wait(535).to({alpha:0},14).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(153,-263.7,2054.5,2311.7999999999997);
+// library properties:
+lib.properties = {
+	id: '9B5EAC1AB2D149FF830E191C51C20FE6',
+	width: 1280,
+	height: 800,
+	fps: 24,
+	color: "#292442",
+	opacity: 1.00,
+	manifest: [],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['9B5EAC1AB2D149FF830E191C51C20FE6'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+an.handleFilterCache = function(event) {
+	if(!event.paused){
+		var target = event.target;
+		if(target){
+			if(target.filterCacheList){
+				for(var index = 0; index < target.filterCacheList.length ; index++){
+					var cacheInst = target.filterCacheList[index];
+					if((cacheInst.startFrame <= target.currentFrame) && (target.currentFrame <= cacheInst.endFrame)){
+						cacheInst.instance.cache(cacheInst.x, cacheInst.y, cacheInst.w, cacheInst.h);
+					}
+				}
+			}
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
