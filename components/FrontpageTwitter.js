3e2743045f6af48fdb0329c75fc7e7f38e61a6dd
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-05-24 15:05:31 +0300

    update twitter intial display

--- components/FrontpageTwitter.js
+++ components/FrontpageTwitter.js
@@ -2,26 +2,34 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import general from '@components/styles/FrontpageGeneral.module.css';
 import twitter from '@components/styles/Twitter.module.css';
 import useWindowSize from '@components/hooks/useWindowSize';
+import twitterSample from '@components/twitterSample.json'
 
 export default function FrontpageTwitter() {
   const size = useWindowSize();
   const ref = useRef();
   const [tweets, setTweets] = useState([]);
-
   useEffect(() => {
-    fetch('/get_tweets')
+    try {
+      fetch('/get_tweets')
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setTweets(data);
       });
+    } catch(error) {
+      console.log("Twitter connection down...")
+    }
   }, []);
 
   return (
     <div className={general.pageHeightBox}>
+      Twitter
       <div className={twitter.twittercontainer}>
+        {/**local testing: replace "tweets" with "twitterSample" */}
         {tweets.map((tweet) => (
-          <h4>{tweet.text}</h4>
+          <div>
+            <a>{tweet.created_at.split('T')[0]}</a>
+            <p>{tweet.text}</p>
+          </div>
         ))}
       </div>
     </div>
