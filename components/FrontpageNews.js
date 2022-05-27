0e1662c7a91f94e611f2f3f899ad06e5f7ef18cb
Tuukka Mattlar <tuukka.mattlar@gmail.com>
2022-05-27 14:22:09 +0300

    fix some small bugs in frontpage layout and news

--- components/FrontpageNews.js
+++ components/FrontpageNews.js
@@ -7,9 +7,9 @@ import styles from './styles/FrontpageCompetences.module.css';
 export default function FrontpageNews({lan}) {
   return (
     <div className={general.pageHeightBox}>
-        <div className={styles.container}>
-        <h1 className={styles.heading}>{{"fi": "Tuoreimpia kuulumisia", "en": "Latest news"}[lan]}</h1>
-        <BlogBoxes lan={lan} maxLen={4} />
+        <div className={styles.containerNews}>
+          <h1 className={styles.heading}>{{"fi": "Tuoreimpia kuulumisia", "en": "Latest news"}[lan]}</h1>
+          <BlogBoxes lan={lan} maxLen={4} />
         </div>
     </div>
   );
